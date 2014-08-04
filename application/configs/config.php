ec2898fb43a6161f779c759886705fed1cf91f6e
= <=>
2014-08-04 12:46:51 +0200

    Update di configs

--- application/configs/config.php
+++ application/configs/config.php
@@ -2,10 +2,10 @@
 
 use DI\Container;
 
-return array(
+return [
     'Extlib\System' => \DI\factory(function (Container $c) {
         return \Extlib\System::getInstance();
     }),
     'Extlib\System\Cookie' => \DI\object('Extlib\System\Cookie')->constructor('system-cookie'),
     'System\Session' => \DI\object('Zend_Session_Namespace')->constructor('system-session')
-);
+];
