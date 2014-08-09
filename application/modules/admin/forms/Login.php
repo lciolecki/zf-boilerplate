6a1fd192abb95d4ec026b877f07a5e1bada0632c
Lukasz Ciolecki <lukasz.ciolecki@gmail.com>
2014-08-09 19:25:37 +0200

    Add base authorization plugin

--- application/modules/admin/forms/Login.php
+++ application/modules/admin/forms/Login.php
@@ -8,7 +8,7 @@ class Admin_Form_Login extends Twitter_Form
 
     public function init()
     {
-        $this->setAttrib('inline', true);
+        //$this->setType(self::FORM_TYPE_HORIZONTAL);
 
         $this->setMethod('post');
         $this->addAttribs(array(
@@ -17,6 +17,7 @@ class Admin_Form_Login extends Twitter_Form
         ));
 
         $this->addElement('text', 'email', array(
+            //'label' => 'User login / email',
             'required' => true,
             'value' => $this->getCookie()->get('email'),
             'filters' => array('StringTrim', array('StringToLower', array('UTF-8'))),
@@ -28,6 +29,7 @@ class Admin_Form_Login extends Twitter_Form
         ));
 
         $this->addElement('password', 'password', array(
+            //'label' => 'User login / email',
             'required' => true,
             'validators' => array(),
             'attribs' => array(
@@ -36,7 +38,7 @@ class Admin_Form_Login extends Twitter_Form
             ),
         ));
 
-//        $this->addElement('checkbox', 'remember', array());
+        $this->addElement('checkbox', 'remember', array('label' => 'Remember me'));
 
         $this->addElement('button', 'submit', array(
             'label' => 'Sign In',
--- src/System/Controller/Plugin/Authorization.php
+++ src/System/Controller/Plugin/Authorization.php
@@ -5,7 +5,7 @@ namespace System\Controller\Plugin;
 use System\Controller\Plugin\AbstractPlugin;
 
 /**
- * ZF-Boilerplate module for checkout localisation
+ * ZF-Boilerplate module for checkout auhorization
  *
  * @category System
  * @package System\Controller
@@ -14,5 +14,63 @@ use System\Controller\Plugin\AbstractPlugin;
  */
 class Authorization extends AbstractPlugin
 {
+    /**
+     * Instance of Zend_Controller_Action_Helper_Redirector
+     *
+     * @var Zend_Controller_Action_Helper_Redirector
+     */
+    protected $redirector = null;
 
+    /**
+     *  Instance of construct
+     */
+    public function __construct()
+    {
+        $this->redirector = new \Zend_Controller_Action_Helper_Redirector();
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see Zend_Controller_Plugin_Abstract::preDispatch()
+     */
+    public function preDispatch(\Zend_Controller_Request_Abstract $request)
+    {
+        $role = 'guest';
+        if ($this->getAuthUser()) {
+            $role = 'user';
+        }
+
+        $acl = $this->getAcl();
+        if (!$acl->has($request->getModuleName()) && !$this->getAuthUser()) {
+            return $this->redirector->gotoUrl($this->getView()->url(array('module' => 'admin', 'controller' => 'login', 'action' => 'index'), 'default', true));
+        } elseif (!$acl->has($request->getModuleName())) {
+            throw new \Zend_Controller_Action_Exception(null, 404);
+        }
+
+
+        if (!$acl->isAllowed($role, $request->getModuleName(), $request->getControllerName())) {
+            $this->getSystemSession()->goto = $request->getParams();
+            return $this->redirector->gotoUrl($this->getView()->url(array('module' => 'admin', 'controller' => 'login', 'action' => 'index'), 'default', true));
+        }
+    }
+
+    /**
+     * Get acl object
+     *
+     * @return \Zend_Acl
+     */
+    public function getAcl()
+    {
+        $acl = new \Zend_Acl();
+
+        $acl->addRole(new \Zend_Acl_Role('guest'));
+        $acl->addRole(new \Zend_Acl_Role('user'), 'guest');
+        $acl->addResource(new \Zend_Acl_Resource('default'));
+        $acl->addResource(new \Zend_Acl_Resource('admin'));
+        $acl->allow('guest', 'default');
+        $acl->allow('guest', 'admin', 'login');
+        $acl->allow('user', 'admin');
+
+        return $acl;
+    }
 }
